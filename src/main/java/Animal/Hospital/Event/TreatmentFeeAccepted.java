048dbde2400c0eb039abda0e988c0cd7d81f1a89
dani-vov <dani-vov>
2020-04-21 13:57:10 +0900

    commit

---
+++ src/main/java/Animal/Hospital/Event/TreatmentFeeAccepted.java
@@ -0,0 +1,52 @@
+package Animal.Hospital.Event;
+
+import Animal.Hospital.AbstractEvent;
+
+public class TreatmentFeeAccepted extends AbstractEvent {
+
+    private Long id;
+    private Long reservationId;
+    private Long fee;
+    private String eventType;
+
+    @Override
+    public String getEventType() {
+        return eventType;
+    }
+
+    @Override
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public TreatmentFeeAccepted(Long reservationId, Long fee){
+        super();
+        this.reservationId = reservationId;
+        this.fee = fee;
+        this.eventType  = TreatmentFeeAccepted.class.getSimpleName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+
+    public void setFee(Long fee) {
+        this.fee = fee;
+    }
+}
